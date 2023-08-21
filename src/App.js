e0c274cd8f1db8d14f670532606776e907a0b745
sungjin <sg5359705@gmail.com>
2023-08-21 13:06:24 +0900

    20230821

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
+
+
 
 function App() {
+  // 변수 (변수는 function 과 return 사이에 집어 넣음)
+  const name = "Hong";
+  let age = 30;
+  const naver = {
+    name : "네이버",
+    url : "https://www.naver.com",
+    target : "_blank"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 컴포넌트 */}
+     <Hello></Hello>
+     <Welcome></Welcome>
+     {/* {변수,숫자,문자열} 사용가능 객체(배열)은 안됨*/}
+     <h1> {'이름 : '} {name}, {"나이 : "} {age}</h1>
+     <h1> 이름 : {name}, 나이 {age}</h1>
+     {/* 객체 {객체이름, 속성} */}
+     <h2><a href={naver.url} target={naver.target}>{naver.name}</a></h2>
     </div>
   );
 }
